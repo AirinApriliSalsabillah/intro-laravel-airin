9f15460da76cf2849a6228822689a016546f459e
AirinApriliSalsabillah <salsabillahairinaprilia@gmail.com>
2024-08-21 17:04:57 +0700

    menambahkan tugas 13

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,10 @@ Route::get('/', [HomeController::class, 'home']);
 
 Route::get('/register', [AuthController::class,"register"]);
 Route::get('/welcome', [AuthController::class,"welcome"]);
+
+route::get('/table',function(){
+    return view('table');
+});
+route::get('/datatable',function(){
+    return view('data-table');
+});
